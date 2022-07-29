9259f7a3208e48b926b58d9d9aa04b30ec0b1b25
CrimsonPiik <moebasem@gmail.com>
2022-07-29 04:36:16 +0300

    fixing

--- lib/screen/mainScreen.dart
+++ lib/screen/mainScreen.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_app_ui/widgets/footer.dart';
+import 'package:coffee_app_ui/widgets/header.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -8,7 +9,84 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [Footer()],
+        children: [
+          const Header(),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 100.0, vertical: 20.0),
+            child: Column(
+              children: [
+                Container(
+                  width: double.maxFinite,
+                  height: 2,
+                  color: Colors.white,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        'Accessories',
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                      Text(
+                        'More Accessories ›',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 250,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: 3,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    color: Colors.yellow),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'MINI FIT-S PODS',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 17),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  '12 JD',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 17),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  width: double.maxFinite,
+                  height: 2,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+          const Footer()
+        ],
       ),
     );
   }
